29a73d40c50a991740c0a6b2a9f90630fbf4cedf
Khushi Patel <khushihpatel06@gmail.com>
2026-02-04 15:37:35 +0530

    Initial Project Setup with Virtual Environment and Core ML Libraries

---
+++ src/test_libs.py
@@ -0,0 +1,4 @@
+import numpy as nu
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+print("All ML libraries installed and working!")
---
+++ src/test_setup.py
@@ -0,0 +1,1 @@
+print("ML project environment is ready!")
